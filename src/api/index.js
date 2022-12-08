aecbdc64c5dc27900631fad04d4ad5e3e50285b0
luohao <353010979@qq.com>
2022-12-08 18:22:10 +0800

    绑定search页面数据

--- src/api/index.js
+++ src/api/index.js
@@ -19,3 +19,23 @@ export const reqFloorList = () => mockRequests({
     url: '/floor',
     method: 'get'
 })
+
+
+//获取搜索模块数据 地址：/api/list  post
+/* 
+{
+  "category3Id": "61",
+  "categoryName": "手机",
+  "keyword": "小米",
+  "order": "1:desc",
+  "pageNo": 1,
+  "pageSize": 10,
+  "props": ["1:1700-2799:价格", "2:6.65-6.74英寸:屏幕尺寸"],
+  "trademark": "4:小米"
+}
+*/
+export const reqGetSearchInfo = (params) => requests({
+    url: '/list',
+    method: 'post',
+    data: params
+})
